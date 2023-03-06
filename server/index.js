8836bd2578412f73b03c61e46b606fdcb3247de9
BKQ8503 <sabariswaran.ramesh@cartus.com>
2023-03-07 02:14:36 +0530

    added register and login user

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
-import { Express } from "express";
+import express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
-import morgan from "morgan";
+import morgan from "morgan"; 
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+import router from "./routes/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -16,11 +18,18 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit:"30mb" , extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/", router);
+
+app.get("/", (req, res) => res.status(200).json({message: "its working"}));
+
+
+
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 
@@ -29,7 +38,27 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "public/assets");
     },
-    filename: function (req,file,cb) {
+    filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
 });
+ 
+const upload = multer({ storage });
+
+
+/**ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+
+/**ROUTES */
+
+
+/**MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(PORT, () => console.log(`SERVER PORT : ${PORT}`))
+}).catch((error) => { console.log(`${error} did not connect`); })
