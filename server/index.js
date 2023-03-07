5395158e5f34940c9d031fd9008cbb984235cda5
BKQ8503 <sabariswaran.ramesh@cartus.com>
2023-03-07 22:54:56 +0530

    completed server and started client code

--- server/index.js
+++ server/index.js
@@ -9,7 +9,15 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
-import router from "./routes/auth.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js"
+import postRoutes from "./routes/post.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -24,7 +32,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/", router);
+
 
 app.get("/", (req, res) => res.status(200).json({message: "its working"}));
 
@@ -48,10 +56,12 @@ const upload = multer({ storage });
 
 /**ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-
+app.post("/post", verifyToken, upload.single("picture"), createPost)
 
 /**ROUTES */
-
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /**MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
@@ -60,5 +70,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    app.listen(PORT, () => console.log(`SERVER PORT : ${PORT}`))
+    app.listen(PORT, () => console.log(`SERVER PORT : ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error) => { console.log(`${error} did not connect`); })
